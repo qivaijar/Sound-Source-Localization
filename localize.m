4915e730e0b7c06ffd029b9461f1a3ef94bf6e2b
Rizki Rivai Ginanjar <32627627+qivaijar@users.noreply.github.com>
2017-11-02 20:41:19 +0700

    Change localize to localize1 (based on paper)

--- localize.m
+++
@@ -1,9 +0,0 @@
-function [ds ang]=localize(mic_kiri,mic_kanan,fs)
-%ang = hasil sudut dengan mic kiri sebagai referensi 
-addpath('G:\Bu Irma\Sound Source Localization\HRTF\Data HRTF\Sound')
-mic_kiri=audioread(mic_kiri);
-mic_kanan=audioread(mic_kanan);
-c=34500;   %sound speed, cm/s
-ds=gccphat(mic_kiri,mic_kanan);
-ms=13.72;  %untuk MRT01, 2 kali w, mic spacing dalam cm
-ang=rad2deg(acos(c*ds/(fs*ms)));
