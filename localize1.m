e81118839dccf36ce7dd6f619b522276f2275fed
Rizki Rivai Ginanjar <32627627+qivaijar@users.noreply.github.com>
2017-11-02 20:46:42 +0700

    Add Localize2 (based on paper)

---
+++ localize1.m
@@ -0,0 +1,10 @@
+function [ds ang]=localize1(mic_kiri,mic_kanan,fs)
+%based on paper Sound source localization on Android Smartphone
+%ang = hasil sudut dengan mic kiri sebagai referensi 
+addpath('G:\Bu Irma\Sound Source Localization\HRTF\Data HRTF\Sound')
+mic_kiri=audioread(mic_kiri);
+mic_kanan=audioread(mic_kanan);
+c=34500;   %sound speed, cm/s
+ds=gccphat(mic_kiri,mic_kanan);
+ms=13.72;  %untuk MRT01, 2 kali w, mic spacing dalam cm
+ang=rad2deg(acos(c*ds/(fs*ms)));
---
+++ localize2.m
@@ -0,0 +1,3 @@
+function [ds ang]=localize2(a,mic_kiri,mic_kanan)
+%based on paper Calibration and uncertainty evaluation using monte carlo
+%method of a simple 2D sound Localization System
