52b0040700915a65602349d4082c1a5dd2bf340d
Rizki Rivai Ginanjar <32627627+qivaijar@users.noreply.github.com>
2017-11-02 21:13:46 +0700

    Update localize2

--- localize1.m
+++ localize1.m
@@ -1,7 +1,7 @@
 function [ds ang]=localize1(mic_kiri,mic_kanan,fs)
 %based on paper Sound source localization on Android Smartphone
 %ang = hasil sudut dengan mic kiri sebagai referensi 
-addpath('G:\Bu Irma\Sound Source Localization\HRTF\Data HRTF\Sound')
+addpath('G:\Bu Irma\Sound Source Localization\HRTF\Data HRTF\Sound')% path to hrtf database wave sound
 mic_kiri=audioread(mic_kiri);
 mic_kanan=audioread(mic_kanan);
 c=34500;   %sound speed, cm/s
--- localize2.m
+++ localize2.m
@@ -1,3 +1,10 @@
-function [ds ang]=localize2(a,mic_kiri,mic_kanan)
+function degg=localize2(a,fs,mic_kiri,mic_kanan)
 %based on paper Calibration and uncertainty evaluation using monte carlo
 %method of a simple 2D sound Localization System
+% a refer to length from ear to the inner head
+addpath('G:\Bu Irma\Sound Source Localization\HRTF\Data HRTF\Sound')% path to hrtf database wave sound
+mic_kiri=audioread(mic_kiri);
+mic_kanan=audioread(mic_kanan);
+ITD=gccphat(mic_kiri,mic_kanan);
+c=34500;  %wave sound speed in cm/s
+degg=ITD*c/(2*fs*a);
